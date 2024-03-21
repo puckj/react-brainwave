c907d2195c56acdc433267c26ea12cbf9ffa57ed
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2024-03-21 14:46:39 +0700

    benefits section completed

---
+++ src/components/Heading.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  className?: string;
+  title?: string;
+}
+
+const Heading = ({ className, title }: Props) => {
+  return (
+    <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
+      {title && <h2 className="h2">{title}</h2>}
+    </div>
+  );
+};
+
+export default Heading;
